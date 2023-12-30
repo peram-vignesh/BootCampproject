5a4f3ef3d1467a829cf70394845bdd2966a44b61
peram-vignesh <peramvigneshchowdary@gmail.com>
2023-12-29 16:14:08 -0800

    dec29

--- DOM practice/TicTacToe1.js
+++ DOM practice/TicTacToe1.js
@@ -1,57 +1,69 @@
-var id1=$('#id1')
-var id2=$('#id2')
-var id3=$('#id3')
-var id4=$('#id4')
-var id5=$('#id5')
-var id6=$('#id6')
-var id7=$('#id7')
-var id8=$('#id8')
-var id9=$('#id9')
-var button=$('button')
+var id1=document.querySelector('#id1')
+var id2=document.querySelector('#id2')
+var id3=document.querySelector('#id3')
+var id4=document.querySelector('#id4')
+var id5=document.querySelector('#id5')
+var id6=document.querySelector('#id6')
+var id7=document.querySelector('#id7')
+var id8=document.querySelector('#id8')
+var id9=document.querySelector('#id9')
+var button=document.querySelector('button')
+var boxes=document.querySelectorAll('td')
 function assign (number,id){
     if (number%2==0){
-        id.text('X')
+        id.textContent='X'
     }
     else{
-        id.text('O')
+        id.textContent='O'
     }
+    console.log(id)
 }
 var i=0
 
-id1.click(function (){
-        assign(i,'id1')
+id1.addEventListener('click',function (){
+        assign(i,id1)
         i=i+1
 })
-id2.click(function (){
-        assign(i,'id2')
+id2.addEventListener('click',function (){
+        assign(i,id2)
         i=i+1
 })
-id3.click(function (){
-        assign(i,'id3')
+id3.addEventListener('click',function (){
+        assign(i,id3)
         i=i+1
 })
-id4.click(function (){
-        assign(i,'id4')
+id4.addEventListener('click',function (){
+        assign(i,id4)
         i=i+1
 })
-id5.click(function (){
-        assign(i,'id5')
+id5.addEventListener('click',function (){
+        assign(i,id5)
         i=i+1
 })
-id6.click(function (){
-        assign(i,'id6')
+id6.addEventListener('click',function (){
+        assign(i,id6)
         i=i+1
 })
-id7.click(function (){
-        assign(i,'id7')
+id7.addEventListener('click',function (){
+        assign(i,id7)
         i=i+1
 })
-id8.click(function (){
-        assign(i,'id8')
+id8.addEventListener('click',function (){
+        assign(i,id8)
         i=i+1
 })
-id9.click(function (){
-        assign(i,'id9')
+id9.addEventListener('click',function (){
+        assign(i,id9)
         i=i+1
 })
-   
+button.addEventListener('click',function (){
+    id1.textContent=''
+    id2.textContent=''
+    id3.textContent=''
+    id4.textContent=''
+    id5.textContent=''
+    id6.textContent=''
+    id7.textContent=''
+    id8.textContent=''
+    id9.textContent=''
+})
